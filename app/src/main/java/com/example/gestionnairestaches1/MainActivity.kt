929b7bc471882ce9266844d39bce8f90c69af8f3
yasmine26-KM <kamaraassetou7@gmail.com>
2025-08-20 14:11:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gestionnairestaches1/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.gestionnairestaches1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.gestionnairestaches1.ui.theme.GestionnairesTaches1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GestionnairesTaches1Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    TaskCompletedScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskCompletedScreen() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = "All tasks completed"
+        )
+
+        Text(
+            text = stringResource(R.string.port),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.bort),
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaskCompletedPreview() {
+    GestionnairesTaches1Theme {
+        TaskCompletedScreen()
+    }
+}
